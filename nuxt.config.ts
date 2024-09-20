90f48fc8e1065b16a3b9182e9207388e775bda6e
Bakhrul <bakhrulbila@gmail.com>
2024-09-20 21:34:37 +0700

    add env example

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -46,7 +46,7 @@ export default defineNuxtConfig({
         },
         {
           rel: 'stylesheet',
-          href:'/main.css?v=1.0.1'
+          href:'/main.css?v=1.0.2'
         },
       ],
       script: [{
