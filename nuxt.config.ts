08feb1874ae59dd6674d34f0b0664e31cd66a289
Bakhrul <bakhrulbila@gmail.com>
2024-09-27 22:05:59 +0700

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,32 +3,30 @@ export default defineNuxtConfig({
   ssr: true,
   compatibilityDate: '2024-04-03',
   gtm: {
-    id: 'GTM-PNXC9PQ8', // Your GTM single container ID, array of container ids ['GTM-xxxxxx', 'GTM-yyyyyy'] or array of objects [{id: 'GTM-xxxxxx', queryParams: { gtm_auth: 'abc123', gtm_preview: 'env-4', gtm_cookies_win: 'x'}}, {id: 'GTM-yyyyyy', queryParams: {gtm_auth: 'abc234', gtm_preview: 'env-5', gtm_cookies_win: 'x'}}], // Your GTM single container ID or array of container ids ['GTM-xxxxxx', 'GTM-yyyyyy']   
+    id: 'GTM-MK5MDV9B', // Your GTM single container ID, array of container ids ['GTM-xxxxxx', 'GTM-yyyyyy'] or array of objects [{id: 'GTM-xxxxxx', queryParams: { gtm_auth: 'abc123', gtm_preview: 'env-4', gtm_cookies_win: 'x'}}, {id: 'GTM-yyyyyy', queryParams: {gtm_auth: 'abc234', gtm_preview: 'env-5', gtm_cookies_win: 'x'}}], // Your GTM single container ID or array of container ids ['GTM-xxxxxx', 'GTM-yyyyyy']   
     defer: false, // Script can be set to `defer` to speed up page load at the cost of less accurate results (in case visitor leaves before script is loaded, which is unlikely but possible). Defaults to false, so the script is loaded `async` by default
-    compatibility: false, // Will add `async` and `defer` to the script tag to not block requests for old browsers that do not support `async`
-    nonce: '2726c7f26c', // Will add `nonce` to the script tag
+    compatibility: false, // Will add `async` and `defer` to the script tag to not block requests for old browsers that do not support `async`    
     enabled: true, // defaults to true. Plugin can be disabled by setting this to false for Ex: enabled: !!GDPR_Cookie (optional)
-    debug: true, // Whether or not display console logs debugs (optional)
+    debug: false, // Whether or not display console logs debugs (optional)
     loadScript: true, // Whether or not to load the GTM Script (Helpful if you are including GTM manually, but need the dataLayer functionality in your components) (optional)
     enableRouterSync: true, // Pass the router instance of your app to automatically sync with router (optional)    
     trackOnNextTick: false, // Whether or not call trackView in Vue.nextTick
-    devtools: true, // (optional)
+    devtools: false, // (optional)
   },
   runtimeConfig: {
     public: {
       API_URL: process.env.API_URL,
       API_KEY: process.env.API_KEY,
       gtm: {
-        id: 'GTM-PNXC9PQ8',      
-        defer: false,
-        compatibility: false,
-        nonce: '2726c7f26c',
-        enabled: true,
-        debug: true,
-        loadScript: true,
-        enableRouterSync: true,        
-        trackOnNextTick: false,
-        devtools: true,
+        id: 'GTM-MK5MDV9B', // Your GTM single container ID, array of container ids ['GTM-xxxxxx', 'GTM-yyyyyy'] or array of objects [{id: 'GTM-xxxxxx', queryParams: { gtm_auth: 'abc123', gtm_preview: 'env-4', gtm_cookies_win: 'x'}}, {id: 'GTM-yyyyyy', queryParams: {gtm_auth: 'abc234', gtm_preview: 'env-5', gtm_cookies_win: 'x'}}], // Your GTM single container ID or array of container ids ['GTM-xxxxxx', 'GTM-yyyyyy']   
+        defer: false, // Script can be set to `defer` to speed up page load at the cost of less accurate results (in case visitor leaves before script is loaded, which is unlikely but possible). Defaults to false, so the script is loaded `async` by default
+        compatibility: false, // Will add `async` and `defer` to the script tag to not block requests for old browsers that do not support `async`    
+        enabled: true, // defaults to true. Plugin can be disabled by setting this to false for Ex: enabled: !!GDPR_Cookie (optional)
+        debug: false, // Whether or not display console logs debugs (optional)
+        loadScript: true, // Whether or not to load the GTM Script (Helpful if you are including GTM manually, but need the dataLayer functionality in your components) (optional)
+        enableRouterSync: true, // Pass the router instance of your app to automatically sync with router (optional)    
+        trackOnNextTick: false, // Whether or not call trackView in Vue.nextTick
+        devtools: false, // (optional)
       }
     }    
   },
