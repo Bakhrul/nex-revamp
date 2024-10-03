ff0042b87200b68f1b0b98c637c7449aef980bc5
Bakhrul <bakhrulbila@gmail.com>
2024-10-03 09:01:18 +0700

    size favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -40,6 +40,7 @@ export default defineNuxtConfig({
         {
           rel: 'icon',
           type: 'image/png',
+          sizes: "any",
           href: '/icon.png'
         },             
         {
