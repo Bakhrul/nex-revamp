23991b220435b34ca2bbdd100e93dc95694a8e3b
Bakhrul <bakhrulbila@gmail.com>
2024-10-10 09:39:55 +0700

    push

--- components/Footer.vue
+++ components/Footer.vue
@@ -31,29 +31,29 @@
                         <div class="d-flex flex-wrap mb-5" style="font-size: 24px">
                             <div class="me-2"><a href="https://id-id.facebook.com/groups/180272675720045/"
                                     style="color:black;text-decoration: none !important">
-                                    <NuxtImg loading="lazy" src="/icon/facebook.svg" alt="facebook" style="width:29px" />
+                                    <NuxtImg src="/icon/facebook.svg" alt="facebook" style="width:29px" />
                                 </a>
                             </div>
 
                             <div class="me-2"><a href="https://x.com/nexofficialtv?s=21"
                                     style="color:black;text-decoration: none !important">
-                                    <NuxtImg loading="lazy" src="/icon/twitter.svg" alt="twitter" style="width:29px" />
+                                    <NuxtImg src="/icon/twitter.svg" alt="twitter" style="width:29px" />
                                 </a>
                             </div>
                             <div class="me-2"><a href="https://z-p42.www.instagram.com/nexparabola_tv/?hl=da"
                                     style="color:black;text-decoration: none !important">
-                                    <NuxtImg loading="lazy" src="/icon/instagram.svg" alt="instagram" style="width:29px" />
+                                    <NuxtImg src="/icon/instagram.svg" alt="instagram" style="width:29px" />
                                 </a>
                             </div>
 
                             <div class="me-2"><a href="https://www.tiktok.com/@nexparabola_tv"
                                     style="color:black;text-decoration: none !important">
-                                    <NuxtImg loading="lazy" src="/icon/tiktok.svg" alt="tiktok" style="width:29px" />
+                                    <NuxtImg src="/icon/tiktok.svg" alt="tiktok" style="width:29px" />
                                 </a>
                             </div>
                             <div><a href="https://www.youtube.com/c/OfficialNexParabolaTV"
                                     style="color:black;text-decoration: none !important">
-                                    <NuxtImg loading="lazy" src="/icon/youtube.svg" alt="youtube" style="width:29px" />
+                                    <NuxtImg src="/icon/youtube.svg" alt="youtube" style="width:29px" />
                                 </a>
                             </div>
 
@@ -61,14 +61,14 @@
                     </div>
                     <div class="footer-title mb-2">Butuh Bantuan?</div>
                     <ul style="font-size:16px" class="mb-1">
-                        <li><a href="mailto:cs@nexparabola.co.id" class="footer-link"> <NuxtImg loading="lazy" alt="email"
+                        <li><a href="mailto:cs@nexparabola.co.id" class="footer-link"> <NuxtImg alt="email"
                                     src="/icon/email.svg" style="width:29px" />
                                 &ensp;cs@mynex.co.id</a></li>
-                        <li><a href="https://wa.me/6281260012700" class="footer-link"> <NuxtImg loading="lazy"
+                        <li><a href="https://wa.me/6281260012700" class="footer-link"> <NuxtImg
                                     src="/icon/whatsapp.svg" alt="whatsapp" style="width:29px" />
                                 &ensp;+62 81260012700</a></li>
                         <li><a href="tel:02180637388">
-                                <div class="footer-link d-flex align-items-start"> <NuxtImg loading="lazy" src="/icon/phone.svg" alt="phone"
+                                <div class="footer-link d-flex align-items-start"> <NuxtImg src="/icon/phone.svg" alt="phone"
                                         style="width:29px" />
                                     <div class="ms-2">(021) 8063 7388<br>
                                         Jam Operasional<br>
@@ -78,7 +78,7 @@
                                     </div>
                                 </div>
                             </a></li>
-                        <li><a href="https://maps.app.goo.gl/7hR2qqfsMb1zGKK97" class="footer-link"> <NuxtImg loading="lazy" src="/icon/location.svg" alt="location"
+                        <li><a href="https://maps.app.goo.gl/7hR2qqfsMb1zGKK97" class="footer-link"> <NuxtImg src="/icon/location.svg" alt="location"
                                     style="width:29px" />
                                 &ensp;PT Mediatama Televisi</a></li>
                     </ul>
@@ -87,29 +87,29 @@
                         <div class="d-flex flex-wrap" style="font-size: 24px">
                             <div class="me-2"><a href="https://id-id.facebook.com/groups/180272675720045/"
                                     style="color:black;text-decoration: none !important">
-                                    <NuxtImg loading="lazy" src="/icon/facebook.svg" alt="facebook" style="width:29px" />
+                                    <NuxtImg src="/icon/facebook.svg" alt="facebook" style="width:29px" />
                                 </a>
                             </div>
 
                             <div class="me-2"><a href="https://x.com/nexofficialtv?s=21"
                                     style="color:black;text-decoration: none !important">
-                                    <NuxtImg loading="lazy" src="/icon/twitter.svg" alt="twitter" style="width:29px" />
+                                    <NuxtImg src="/icon/twitter.svg" alt="twitter" style="width:29px" />
                                 </a>
                             </div>
                             <div class="me-2"><a href="https://z-p42.www.instagram.com/nexparabola_tv/?hl=da"
                                     style="color:black;text-decoration: none !important">
-                                    <NuxtImg loading="lazy" src="/icon/instagram.svg" alt="instagram" style="width:29px" />
+                                    <NuxtImg src="/icon/instagram.svg" alt="instagram" style="width:29px" />
                                 </a>
                             </div>
 
                             <div class="me-2"><a href="https://www.tiktok.com/@nexofficialtv"
                                     style="color:black;text-decoration: none !important">
-                                    <NuxtImg loading="lazy" src="/icon/tiktok.svg" alt="tiktok" style="width:29px" />
+                                    <NuxtImg src="/icon/tiktok.svg" alt="tiktok" style="width:29px" />
                                 </a>
                             </div>
                             <div><a href="https://www.youtube.com/c/OfficialNexParabolaTV"
                                     style="color:black;text-decoration: none !important">
-                                    <NuxtImg loading="lazy" src="/icon/youtube.svg" alt="youtube" style="width:29px" />
+                                    <NuxtImg src="/icon/youtube.svg" alt="youtube" style="width:29px" />
                                 </a>
                             </div>
 
@@ -120,12 +120,12 @@
                     <div class="row">
                         <div class="col-lg-12 col-5 mb-5">
                             <div class="footer-title mb-2">Payment Partners</div>
-                            <NuxtImg loading="lazy" src="/payment.svg" alt="payment" style="max-width:100%"/>
+                            <NuxtImg src="/payment.svg" alt="payment" style="max-width:100%"/>
                         </div>
                         <div class="col-lg-12 col-7 mb-5">
                             <div class="footer-title mb-2">Download Apps Nex</div>
                             <div class="d-flex align-items-center">
-                                <NuxtImg loading="lazy" format="webp" src="/sample-qr.png" alt="qr" class="me-2" style="max-width:80px"/>
+                                <NuxtImg format="webp" src="/sample-qr.png" alt="qr" class="me-2" style="max-width:80px"/>
                                 <a class="w-100"
                                     href="https://play.google.com/store/apps/details?id=ezlink.project.nexparabola&pli=1"><NuxtImg loading="lazy" format="webp"
                                         src="/google-play.png" alt="google-play"
